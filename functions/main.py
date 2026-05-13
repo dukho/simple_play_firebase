57a089f6f444dcaf6062539df5b4dab9a1afc3eb
Dukho Choi <dchoi@woolworths.com.au>
2026-05-13 16:04:28 +1000

    Update gitignore

--- functions/main.py
+++ functions/main.py
@@ -16,6 +16,6 @@ set_global_options(max_instances=10)
 # initialize_app()
 #
 #
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
