48a8b7539e316e858ff29fd2d45c6aa24a006e15
Dukho Choi <dchoi@woolworths.com.au>
2026-05-14 16:42:00 +1000

    Use Flask for functions

--- functions/main.py
+++ functions/main.py
@@ -1,21 +1,18 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
 from firebase_functions.options import set_global_options
 from firebase_admin import initialize_app
-
-# For cost control, you can set the maximum number of containers that can be
-# running at the same time. This helps mitigate the impact of unexpected
-# traffic spikes by instead downgrading performance. This limit is a per-function
-# limit. You can override the limit for each function using the max_instances
-# parameter in the decorator, e.g. @https_fn.on_request(max_instances=5).
-set_global_options(max_instances=10)
+from flask import Flask, jsonify
 
 initialize_app()
-#
-#
+set_global_options(max_instances=10)
+
+flask_app = Flask(__name__)
+
+@flask_app.get("/dev/test")
+def dev_test():
+    return jsonify({"message": "Hello from Flask!"})
+
 @https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+def api(req: https_fn.Request) -> https_fn.Response:
+    with flask_app.request_context(req.environ):
+        return flask_app.full_dispatch_request()
