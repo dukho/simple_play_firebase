80bb910f0071191cc76ccf086fa39394f0598070
Dukho Choi <dchoi@woolworths.com.au>
2026-05-15 13:21:50 +1000

    Add reading bigquery table experiment code

--- functions/main.py
+++ functions/main.py
@@ -2,11 +2,14 @@ from firebase_functions import https_fn
 from firebase_functions.options import set_global_options
 from firebase_admin import initialize_app
 from flask import Flask, jsonify
+from google.cloud import bigquery
 
 initialize_app()
 set_global_options(max_instances=10)
 
 flask_app = Flask(__name__)
+
+bq_client = bigquery.Client()  # reuse across requests (module-level)
 
 @flask_app.get("/dev/test")
 def dev_test():
@@ -14,7 +17,18 @@ def dev_test():
 
 @flask_app.get("/dev/query")
 def read_query():
-    return jsonify({"message": "For query!"})
+    query = """
+        SELECT
+            event_timestamp,
+            app_build_version,
+            (SELECT value FROM UNNEST(custom_attributes) WHERE key = '_app_start') AS _app_start,
+            (SELECT value FROM UNNEST(custom_attributes) WHERE key = 'app_initial_display') AS app_initial_display
+        FROM `simpleplay-c585b.firebase_performance.com_nomad_simpleplay_ANDROID`
+        LIMIT 10
+    """
+    rows = bq_client.query(query).result()
+    data = [dict(row) for row in rows]
+    return jsonify(data)
 
 @https_fn.on_request()
 def api(req: https_fn.Request) -> https_fn.Response:
