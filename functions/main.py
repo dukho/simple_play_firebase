f16b75e0d69b9c2fda0914ef6fd34ab7d20b78d3
Dukho Choi <dchoi@woolworths.com.au>
2026-05-29 15:56:59 +1000

    Add delta reporting

--- functions/main.py
+++ functions/main.py
@@ -43,7 +43,48 @@ def dev_slack_test():
         return jsonify({"status": "success"}), 200
     else:
         return jsonify({"error": "Failed to send to Slack"}), 500
+
+def report_to_slack(webhook_url, message_json):
+    if not webhook_url:
+        print("Error: Slack Webhook URL secret is missing or not bound.")
+        return
+
+    response = requests.post(webhook_url, json=message_json)
+
+    if response.status_code == 200:
+        return jsonify({"status": "success"}), 200
+    else:
+        return jsonify({"error": "Failed to send to Slack"}), 500
+
+def generate_report(title, data):
+  message = f"Checking {title}"
+
+  if len(data) == 0:
+    message = message + """
+      No data available
+    """
     
+  elif len(data) == 1:
+    entry = data[0]
+    message = message + f"""
+      version {entry['app_display_version']} ({entry['app_build_version']}) time = {entry['duration_ms']:.2f} msec
+    """
+
+  else:
+    for i in range(len(data) - 1):
+      current_entry = data[i]
+      previous_entry = data[i + 1]
+
+      current_time = current_entry['duration_ms']
+      previous_time = previous_entry['duration_ms']
+
+      delta = current_time - previous_time
+      change_percentile = (delta / previous_time) * 100 if previous_time != 0 else float('inf')
+      message = message + f"""
+        version {current_entry['app_display_version']} ({current_entry['app_build_version']}) time = {current_entry['duration_ms']:.2f} msec, delta = {delta:.2f} msec ({change_percentile:.2f}%)
+      """
+
+  return message
 
 @flask_app.get("/dev/query")
 def read_query():
@@ -191,6 +232,84 @@ def read_appinit_p90():
     rows = bq_client.query(query).result()
     data = [dict(row) for row in rows]
     return jsonify(data)
+
+
+# _app_start, report to slack
+@flask_app.get("/dev/appstart_report")
+def read_appstart_report():
+    webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+    if not webhook_url:
+        print("Error: Slack Webhook URL secret is missing or not bound.")
+        return jsonify({"error": "Slack Webhook URL secret is missing or not bound."}), 500
+
+    query = """
+        WITH VersionStats AS (
+            SELECT
+                app_build_version,
+                app_display_version,
+                event_timestamp,
+                -- Calculate the 90th percentile for a realistic user experience metric
+                PERCENTILE_CONT(trace_info.duration_us, 0.9) OVER(PARTITION BY app_build_version) / 1000 AS p90_duration_ms,
+                os_version
+            FROM `simpleplay-c585b.firebase_performance.com_nomad_simpleplay_ANDROID`
+            WHERE
+                event_type = 'DURATION_TRACE'
+                AND event_name ='_app_start'
+                AND event_timestamp > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 60 DAY)
+        )
+        SELECT
+        app_build_version,
+        app_display_version,
+        MAX(p90_duration_ms) as duration_ms
+        FROM VersionStats
+        GROUP BY app_build_version, app_display_version
+        ORDER BY
+            app_build_version DESC,
+            app_display_version DESC
+    """
+    rows = bq_client.query(query).result()
+    data = [dict(row) for row in rows]
+
+    report_message = generate_report("App Start Time (_app_start)", data)
+    return report_to_slack(webhook_url, {"text": report_message})
+
+@flask_app.get("/dev/appinit_report")
+def read_appinit_report():
+    webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+    if not webhook_url:
+        print("Error: Slack Webhook URL secret is missing or not bound.")
+        return jsonify({"error": "Slack Webhook URL secret is missing or not bound."}), 500
+
+    query = """
+        WITH VersionStats AS (
+            SELECT
+                app_build_version,
+                app_display_version,
+                event_timestamp,
+                -- Calculate the 90th percentile for a realistic user experience metric
+                PERCENTILE_CONT(trace_info.duration_us, 0.9) OVER(PARTITION BY app_build_version) / 1000 AS p90_duration_ms,
+                os_version
+            FROM `simpleplay-c585b.firebase_performance.com_nomad_simpleplay_ANDROID`
+            WHERE
+                event_type = 'DURATION_TRACE'
+                AND event_name ='app_initial_display'
+                AND event_timestamp > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 60 DAY)
+        )
+        SELECT
+        app_build_version,
+        app_display_version,
+        MAX(p90_duration_ms) as duration_ms
+        FROM VersionStats
+        GROUP BY app_build_version, app_display_version
+        ORDER BY
+            app_build_version DESC,
+            app_display_version DESC
+    """
+    rows = bq_client.query(query).result()
+    data = [dict(row) for row in rows]
+
+    report_message = generate_report("App Initial Display Time (app_initial_display)", data)
+    return report_to_slack(webhook_url, {"text": report_message})
 
 @https_fn.on_request(secrets=["SLACK_WEBHOOK_URL"])
 def api(req: https_fn.Request) -> https_fn.Response:
